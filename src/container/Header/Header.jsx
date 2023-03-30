e51f2f9b59626014102fa47dbe4658622520de23
Gr34sy <slupek3@poczta.fm>
2023-03-30 11:43:29 +0200

    started header implementation

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { SubHeading } from '../../components/SubHeading';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <header className='app__header app__wrapper section__padding' id="home">
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+
+    <div className="app__wraper_img">
+
+    </div>
+  </header>
 );
 
 export default Header;
